2c6f9b331f10955a8335e00d251ba6f7fd6b2b72
Dmitriy-Tyukh <roost1982@bigmir.net>
2023-02-06 09:08:37 +0200

    add basename fron App

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store } from 'redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/Blended_Lesson_3">
         <App />
       </BrowserRouter>
     </Provider>
